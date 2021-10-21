aa6ac14635c2f4f882ce8c95246e49b84fc4969b
Ryan Brower <ryan.brower28@gmail.com>
2021-10-20 17:48:33 -0700

    base layout

---
+++ mastermind/game/director.py
@@ -0,0 +1,69 @@
+from game.board import Board
+from game.console import Console
+from game.move import Move
+from game.player import Player
+from game.roster import Roster
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        board (Hunter): An instance of the class of objects known as Board.
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue.
+        move (Rabbit): An instance of the class of objects known as Move.
+        roster (Roster): An instance of the class of objects known as Roster.
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+
+    def _prepare_game(self):
+        """Prepares the game before it begins. In this case, that means getting the player names and adding them to the roster.
+        
+        Args:
+            self (Director): An instance of Director.
+        """
+    
+    def _get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means getting the move from the current player.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+
+    def _do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means updating the board with the current move.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+ 
+    def _do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, that means checking if there are stones left and declaring the winner.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+
+     
+       
