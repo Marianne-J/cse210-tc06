e275fcd144d187147c2bae26b528146694cbab61
Marianne Johnson <joh20041@byui.edu>
2021-10-22 19:31:05 -0700

    Added code to init, to_string, and check_win.

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -6,20 +6,58 @@ class Board:
 
 
     def __init__(self):
+        """The class constructor.
         
+        Args:
+            self (Board): an instance of Board.
+        """
         self._code = ""
+        self._guess = ""
+        self._player_1_status = []
+        self._player_2_status = []
+        self._prepare()
 
 
     def apply(self):
         pass
 
 
-    def is_empty(self):
-        pass
+    def check_win(self):
+        """Determines if a player has guessed the code. It returns True if the player has correctly
+        guessed the code; false if otherwise.
+        
+        Args:
+            self (Board): An instance of Board.
+
+        Return:
+            Boolean
+        """
+        if self._guess == self._code:
+            check_win = True
+        else:
+            check_win = False
+        
+        return check_win
 
 
-    def to_string(self):
-        pass
+    def to_string(self, players):
+        """Converts the board data to its string representation and returns it to the caller.
+        
+        Args:
+            self (Board): An instance of Board.
+            players (List): A list of the players currently playing the game.
+
+        Return:
+            String
+        """
+        player_1 = f"Player {players[0]}: {self._player_1_status[0]}, {self._player_1_status[1]}\n"
+        player_2 = f"Player {players[1]}: {self._player_2_status[0]}, {self._player_2_status[1]}\n"
+
+        status = player_1 + player_2
+        
+        current_board = f"\n--------------------\n{status}--------------------\n"
+        
+        return current_board
 
 
     def _prepare(self):
